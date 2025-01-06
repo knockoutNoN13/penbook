b909320da15ef80d2ed5b4a2af94a6c3830546e9
kn0ckout13 <pavelvetrov5725@gmail.com>
2025-01-06 15:19:24 +0300

    Pravki

--- backend/pkg/models/commands.go
+++ backend/pkg/models/commands.go
@@ -1,11 +1,11 @@
 package models
 
 type Command struct {
-	Id         string   `bson:"_id,omitempty"`
-	Name       string   `bson:"name"`
-	Descripton string   `bson:"description"`
-	Template   string   `bson:"template"`
-	Args       []string `bson:"args"`
+	Id          string   `bson:"_id,omitempty"`
+	Name        string   `bson:"name"`
+	Description string   `bson:"description"`
+	Template    string   `bson:"template"`
+	Args        []string `bson:"args"`
 }
 
 type GetAllResponse struct {
